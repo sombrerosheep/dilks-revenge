72fb0360aa76c06c311bd9947b1167fe317ae1b8
sombrerosheep <sombrerosheep@gmail.com>
2023-12-19 20:48:37 -0600

    remove player alloc

--- src/game.c
+++ src/game.c
@@ -11,7 +11,7 @@
 #include <stdio.h>
 
 struct drev_game {
-    Player           *player;
+    Player            player;
     EnemyRailManager *rail_manager;
     GameInput         controller;
 };
@@ -19,9 +19,9 @@ struct drev_game {
 static void Game_Update(Game *game, System *sys, Frame delta) {
     Controller_Update(&game->controller, sys);
 
-    Player_Update(game->player, &game->controller, delta.sec);
+    Player_Update(&game->player, &game->controller, delta.sec);
 
-    EnemyRailManager_SetFocus(game->rail_manager, game->player->position);
+    EnemyRailManager_SetFocus(game->rail_manager, game->player.position);
     EnemyRailManager_Update(game->rail_manager, delta.sec, sys->renderer);
 }
 
@@ -29,7 +29,7 @@ static void Game_Draw(Game *game, System *sys) {
     SDL_SetRenderDrawColor(sys->renderer, 0x33, 0x33, 0x33, 0xFF);
     SDL_RenderClear(sys->renderer);
 
-    Player_Draw(game->player, sys->renderer);
+    Player_Draw(&game->player, sys->renderer);
     EnemyRailManager_Draw(game->rail_manager, sys->renderer);
 
     SDL_RenderPresent(sys->renderer);
@@ -44,14 +44,7 @@ Game *Game_Create(int game_width, int game_height) {
         return NULL;
     }
 
-    if ((g->player = malloc(sizeof(Player))) == NULL) {
-        printf("ERROR :: Unable to allocate memory for player\n");
-        Game_Destroy(g);
-        return NULL;
-    }
-
-    if (Player_Init(g->player, (Vec2){(float)game_width / 2.f, (float)game_height / 2.f}) !=
-        0) {
+    if (Player_Init(&g->player, (Vec2){(float)game_width / 2.f, (float)game_height / 2.f}) != 0) {
         printf("ERROR :: Unable to initialize player\n");
         Game_Destroy(g);
         return NULL;
@@ -126,10 +119,6 @@ void Game_Run(Game *g, System *sys) {
 }
 
 void Game_Destroy(Game *g) {
-    if (g->player != NULL) {
-        free(g->player);
-    }
-
     if (g->rail_manager != NULL) {
         EnemyRailManager_Destroy(g->rail_manager);
     }
