c56f16db9eaf93a4008469711ba3d3595c5d080f
sombrerosheep <sombrerosheep@gmail.com>
2023-12-21 13:48:07 -0600

    version check

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <SDL_version.h>
 #include <stdio.h>
 
 #include "game.h"
@@ -8,10 +9,24 @@
 #define GAME_HEIGHT   600
 #define GAME_WIDTH    800
 
+SDL_version required_sdl = {.major = 2, .minor = 0, .patch = 22};
+
 int main() {
     printf("start\n");
 
     System sys;
+
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_version vers;
+    SDL_GetVersion(&vers);
+    printf("Using SDL Version: %d.%d.%d\n", vers.major, vers.minor, vers.patch);
+
+    if (!SDL_VERSION_ATLEAST(required_sdl.major, required_sdl.minor, required_sdl.patch)) {
+        printf("SDL minimum version not met!\n");
+        printf("\twant: %d.%d.%d\n", required_sdl.major, required_sdl.minor, required_sdl.patch);
+        printf("\thave: %d.%d.%d\n", vers.major, vers.minor, vers.patch);
+        return -1;
+    }
 
     System_Init(&sys, "Dilks Revenge", WINDOW_WIDTH, WINDOW_HEIGHT, GAME_WIDTH, GAME_HEIGHT);
     Game *dr_game = Game_Create(GAME_WIDTH, GAME_HEIGHT);
