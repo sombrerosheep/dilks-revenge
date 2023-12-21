5a4bd2f722e2a5cc9a548458cc3b44e092a899e6
sombrerosheep <sombrerosheep@gmail.com>
2023-12-21 13:48:52 -0600

    collision callbacks

---
+++ include/collisions.h
@@ -0,0 +1,26 @@
+#ifndef DREV_COLLISIONS_H
+#define DREV_COLLISIONS_H
+
+#include "bullet.h"
+#include "enemy.h"
+#include "player.h"
+#include "shield.h"
+#include <SDL.h>
+
+typedef enum {
+    Collider_Bullet = 0,
+    Collider_Player,
+    Collider_Enemy,
+
+    Collider_Count
+} Collider;
+
+int get_collision(const SDL_FRect *a, const SDL_FRect *b, SDL_FRect *collision);
+
+void resolve_collision_bullet_bullet(Bullet *a, Bullet *b);
+void resolve_collision_player_bullet(Player *p, Bullet *b);
+void resolve_collision_enemy_bullet(Enemy *e, Bullet *b);
+void resolve_collision_shield_bullet(Shield *s, Bullet *b);
+void resolve_collision_player_enemy(Player *p, Enemy *e);
+
+#endif // DREV_COLLISIONS_H
---
+++ src/collisions.c
@@ -0,0 +1,65 @@
+#include "collisions.h"
+#include "bullet.h"
+
+// todo: inline
+static float reduce_health(float start, float by) {
+    return SDL_max((start - by), 0.f);
+}
+
+int get_collision(const SDL_FRect *a, const SDL_FRect *b, SDL_FRect *collision) {
+    if (SDL_IntersectFRect(a, b, collision) == SDL_TRUE) {
+        return 1;
+    }
+
+    return 0;
+}
+
+void resolve_collision_bullet_bullet(Bullet *a, Bullet *b) {
+    if (a->health > b->health) {
+        a->health = reduce_health(a->health, b->health);
+        b->health = 0.f;
+    } else {
+        b->health -= reduce_health(b->health, a->health);
+        a->health = 0.f;
+    }
+
+    return;
+}
+
+void resolve_collision_player_bullet(Player *p, Bullet *b) {
+    if (b->type == BulletType_Player) {
+        return;
+    }
+
+    p->health = reduce_health(p->health, b->health);
+    b->health = 0.f;
+
+    return;
+}
+
+void resolve_collision_enemy_bullet(Enemy *e, Bullet *b) {
+    if (b->type == BulletType_Enemy) {
+        return;
+    }
+
+    e->health = reduce_health(e->health, b->health);
+    b->health = 0.f;
+
+    return;
+}
+
+void resolve_collision_shield_bullet(Shield *s, Bullet *b) {
+    if (b->type == BulletType_Player) {
+        return;
+    }
+
+    s->health = reduce_health(s->health, b->health);
+    b->health = 0.f;
+
+    return;
+}
+
+void resolve_collision_player_enemy(Player *p, Enemy *e) {
+    p->health = 0.f;
+    e->health = 0.f;
+}
